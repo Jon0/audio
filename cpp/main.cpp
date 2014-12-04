a347b2d0723ef8d26dd9253efce47d6f576d8ac3
Jon0 <Jono4728@gmail.com>
2014-12-04 22:07:37 +1300

    remove unused patterns

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -17,33 +17,38 @@
 
 using namespace std;
 
+/*
+ * some other interesting patterns:
+ *
+ * short s1 = (td*(td >> 5 | td >> 8))>>(td>>16);
+ * short s1 =  6 * (td|td>>7|td>>9)+
+ * 			4 * (td&td>>13|td>>9)+
+ * 			2 * ((td+64)|td>>27|td>>9);
+ * 
+ * short s1 = (td>>7|td|td>>6)*6+4*(td&td>>13|td>>6);
+ * 
+ * short s1 = (td|td>>7|(td>>6 % 3)) + ((td+64)|td>>27|(td>>9 % 3));
+ * 
+ * short s1 =  ((td>>5|td|td>>9)*6^4*(td&td>>11|td>>9))>>((td>>11)*(td>>13) % 3);
+*/
+
 int main(int argc, char *argv[]) {
 	srand( time( NULL ) );
+
+	// TODO: visualisation
 	// glutInit(&argc, argv);
 	//new MainWindow(800, 600);
 	//source *b = new TestBuffer();
-
 	//Scale s; s.get(t);
 
 	function<short(long)> f = [&](long t) -> short {
+		// adjust sample rate
 		long td = t / 3;
 
-		short s1 = (td*(td >> 5 | td >> 8))>>(td>>16);
-		short s2 =  6 * (td|td>>7|td>>9)+
-					4 * (td&td>>13|td>>9)+
-					2 * ((td+64)|td>>27|td>>9);
+		short s1 =  (((td>>5|td|td>>9)*6^4*(td&td>>11|td>>9))>>((td>>11)*(td>>13) % 3))|(td>>(13-((td>>7)*(td>>23) % 13)));
 
-		short s3 = (td>>7|td|td>>6)*6+4*(td&td>>13|td>>6);
-
-
-
-		short s4 = (td|td>>7|(td>>6 % 3)) + ((td+64)|td>>27|(td>>9 % 3));
-
-		short s5 =  ((td>>5|td|td>>9)*6^4*(td&td>>11|td>>9))>>((td>>11)*(td>>13) % 3);
-
-		short s6 =  (((td>>5|td|td>>9)*6^4*(td&td>>11|td>>9))>>((td>>11)*(td>>13) % 3))|(td>>(13-((td>>7)*(td>>23) % 13)));
-
-		return (s6 & 0xff) * 200;
+		// 8 bit output with volume adjustment
+		return (s1 & 0xff) * 200;
 	};
 
 	source *b = new FunctionBuffer(f);
